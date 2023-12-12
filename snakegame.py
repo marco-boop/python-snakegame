168b645e13a39133cebb022f3a96690ef2d33fca
Marco <sanchez.junco@gmail.com>
2023-12-12 13:47:36 -0800

    fix directions

--- snakegame.py
+++ snakegame.py
@@ -37,16 +37,20 @@ segments = []
 
 # Functions
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 def move():
     if head.direction == "up":
@@ -75,6 +79,8 @@ wn.onkeypress(go_right, "d")
 # Main game loop
 while True:
     wn.update()
+
+
 
     # Check for collision with border
     if head.xcor()> 290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
@@ -117,6 +123,19 @@ while True:
         segments[0].goto(x, y)
     
     move()
+
+    # Check for head collisions with the body segments
+    for segment in segments:
+        if segment.distance(head)<20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000,1000)
+
+            # Clear the segments list
+            segments.clear()
 
     time.sleep(delay)
 
